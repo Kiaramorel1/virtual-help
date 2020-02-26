7a19c9a91e83beb3b4ff01cd8f434768b2e477e5
lbirts <birtslauren@gmail.com>
2020-02-26 16:16:50 -0500

    external js

---
+++ student-course-page/assets/course.js
@@ -0,0 +1,21 @@
+// Sidebar Toggle
+$(document).ready(function () {
+    // Toggle Sidebar
+    $('#menu-toggle').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    // dropdown in Sidebar
+    $('#1submenu').on('click', function () {
+        $('.dropdown-container1').toggle();
+    })
+    $('#2submenu').on('click', function () {
+        $('.dropdown-container2').toggle();
+    })
+    $('#3submenu').on('click', function () {
+        $('.dropdown-container3').toggle();
+    })
+    $('#4submenu').on('click', function () {
+        $('.dropdown-container4').toggle();
+    })
+
+});
