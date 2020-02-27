60d8020c9822405592e6fefa19ce27f977e529b6
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-02-27 10:05:38 -0500

    NEW FILE
    NEW FILE

--- student-course-page/assets/course.js
+++ student-course-page/assets/course.js
@@ -2,8 +2,8 @@
 $(document).ready(function () {
     // Toggle Sidebar
     $('#menu-toggle').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+        $('#sidebar').toggleClass('toggle');
+    })
     // dropdown in Sidebar
     $('#1submenu').on('click', function () {
         $('.dropdown-container1').toggle();
@@ -17,5 +17,21 @@ $(document).ready(function () {
     $('#4submenu').on('click', function () {
         $('.dropdown-container4').toggle();
     })
-
+    $('#menu-toggle').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    })
+    // // Accordian Collapse
+    // $(document).ready(function(){
+    //     // Add minus icon for collapse element which is open by default
+    //     $(".collapse.show").each(function(){
+    //     	$(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+    //     });
+        
+    //     // Toggle plus minus icon on show hide of collapse element
+    //     $(".collapse").on('show.bs.collapse', function(){
+    //     	$(this).prev(".button-link").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+    //     }).on('hide.bs.collapse', function(){
+    //     	$(this).prev(".button-link").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    //     });
+    // });
 });
